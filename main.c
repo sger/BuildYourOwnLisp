3ce60e2374b6c01c855c45194bc8ce60142a3f28
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-02-01 20:41:33 +0200

    comments

--- main.c
+++ main.c
@@ -5,8 +5,10 @@
 
 #include "mpc.h"
 
+/*Create Enumeration of Possible lval Types*/
 enum { LVAL_NUM, LVAL_ERR };
 
+/*Create Enumeration of Possible Error Types*/
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
 /*Declare New lval Struct*/
